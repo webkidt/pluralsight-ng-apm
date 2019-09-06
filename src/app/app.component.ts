7310a4c72a3c2182c02ee94fd48e41789f6c541a
David Pam <niratpam@gmail.com>
2019-09-06 04:08:44 +0100

    APM Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'pm-root',
+    template: `
+        <div>
+            <h1>{{ pageTitle }}</h1>
+            <div>My First Component</div>
+        </div>
+    `
 })
 export class AppComponent {
-  title = 'APM';
+    pageTitle = 'Acme Product Managment';
 }
