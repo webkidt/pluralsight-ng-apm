0aae0187212af3c4c8599db49936c8656817cbdb
David Pam <niratpam@gmail.com>
2019-09-07 05:12:32 +0100

    Services and Retrieving Data with HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
@@ -18,7 +19,8 @@ import { StarComponent } from './shared/star.component';
     ],
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        HttpClientModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { IProduct } from './product';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+    private productUrl = 'api/products/products.json';
+
+    constructor(private http: HttpClient) { }
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+            tap(data => console.log('All: ' + JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        // in a real world app, we may send the server some remote logging infastructure
+        // instead of just logging it to a console.
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+            // A client-side network error occured. Handle it accordinly.
+            errorMessage = `An error occured: ${err.error.message}`;
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong.
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+    }
+}
