4758a928706f47b452b63c5364a6c519ced3aac2
David Pam <niratpam@gmail.com>
2019-09-15 00:47:30 +0100

    Update ProductDetailComponent

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { IProduct } from './product';
+import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,25 +10,27 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
-  product: IProduct;
+  errorMessage = '';
+  product: Product | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    private productService: ProductService) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
-    this.product = {
-      productId: id,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2019',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png'
-    };
+    const param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
   }
 
   onBack(): void {
--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,15 +1,3 @@
-export interface IProduct {
-  productId: number;
-  productName: string;
-  productCode: string;
-  tags?: string[];
-  releaseDate: string;
-  price: number;
-  description: string;
-  starRating: number;
-  imageUrl: string;
-}
-
 export interface Product {
   id: number;
   productName: string;
